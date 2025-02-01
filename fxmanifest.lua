80d8410a1ef4982d926fcd6612ab7a89afb2305a
xSnufy <99494681+xSnufy@users.noreply.github.com>
2025-02-01 02:29:29 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'NS Script'
+description 'ns_weaponcraft (https://discord.gg/S8aMUQvsCr)'
+version '1.0.0'
+
+client_scripts {
+    'main.lua'
+}
+
+dependencies {
+    'es_extended'
+}
